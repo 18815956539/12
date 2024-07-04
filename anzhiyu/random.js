fc9287f8862e3464bb8e9be6f23e930ebfcbccaa
“18815956539” <chenjianan20110607@outlook.com>
2024-07-05 07:49:58 +0800

    Site updated: 2024-07-05 07:49:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/30/hello-world/","2024/06/30/各大视频网站地址/"];function toRandomPost(){
+var posts=["2024/07/05/hello-world/","2024/07/05/7月5号视频附件表-SYSTEAM-PLAY/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
